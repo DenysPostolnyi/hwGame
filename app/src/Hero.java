0a247ea0178c9a795e565617e41877c1c4e6dd1a
Denys Postolnyi <postlden44@gmail.com>
2022-10-30 14:12:00 +0200

    First task

---
+++ app/src/Hero.java
@@ -0,0 +1,15 @@
+public class Hero {
+    private final String name;
+
+    public Hero(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void attackEnemy(){
+        System.out.println("Enemy attacked");
+    }
+}
---
+++ app/src/TrainingGround.java
@@ -0,0 +1,7 @@
+public class TrainingGround {
+    public static void main(String[] args) {
+        Hero hero = new Hero("Super Jack");
+        System.out.println("Hero's name: " + hero.getName());
+        hero.attackEnemy();
+    }
+}
